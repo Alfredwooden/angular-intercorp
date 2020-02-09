a123f9d670d41d2613b6efd81469b71a2e1e8db3
Alfredo Barros <coomcs@gmail.com>
2020-02-09 19:18:25 -0300

    added customer list components with logic (#4)

--- src/app/modules/home/pages/customer/components/index.ts
+++ src/app/modules/home/pages/customer/components/index.ts
@@ -1,5 +1,7 @@
-import { NewCustomerComponent } from "./new-customer/new-customer.component";
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { NewCustomerComponent } from './new-customer/new-customer.component';
 
 export const CUSTOMER_COMPONENTS = [
-  NewCustomerComponent
+  NewCustomerComponent,
+  CustomerListComponent
 ]
